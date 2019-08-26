79955919a63d24d1fcd21109ca9800c043f520c7
Jess Madeux <jessmadeux@Jesss-MacBook-Pro.local>
2019-08-26 10:22:27 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFouro = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
