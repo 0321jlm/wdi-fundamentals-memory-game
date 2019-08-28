5d7843b4aa4d8c03cd4d739e8a48ed74b816e10d
0321jlm <jessica.madeux@prudential.com>
2019-08-28 10:30:27 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,16 +4,23 @@
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length===2) {
+function checkForMatch() {
+
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else {alert ("Sorry. try again.");}
-	}
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry. try again.");
+	} 
+}	
 
-console.log(cardTwo);
+function flipCard(cardID) {
+     console.log ("User flipped " + cards[cardID]);
+     cardsInPlay.push(cards[cardID]);
+
+     
+}
+     	flipCard(0);
+		flipCard(1);
+		checkForMatch();
+
